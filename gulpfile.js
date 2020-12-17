bb207d44c2dd48407a36a5df8a44d3f22e009381
AlekseyVerba <leha.verba.01@mail.ru>
2020-12-17 23:21:22 +0800

    obnova

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,3 @@
-const {src, dest, watch, parallel, series} = require("gulp");
-
-const scss = require("gulp-sass");
-const concat = require("gulp-concat");
-const browserSync = require("browser-sync");
-const uglify = require("gulp-uglify-es").default;
-const autoprefixer = require("gulp-autoprefixer");
-const imagemin = require("gulp-imagemin");
-const del = require("del");
-const tildeImporter = require('node-sass-tilde-importer');
-const webpack = require('webpack-stream');
-
-
-function build() {
-    return src([
-        "src/css/style.min.css",
-        "src/fonts/**/*",
-        "src/js/script.min.js",
-        "src/*.html"
-    ], {base: "src"})
-        .pipe(dest("dist"));
-}
-
 function cleanDist() {
     return del("dist");
 }
@@ -50,6 +27,9 @@ function scripts() {
         .pipe(webpack({
             // Any configuration options...
         }))
+        .pipe(babel({
+            presets: ["@babel/preset-env"]
+          }))
         .pipe(concat("script.min.js"))
         .pipe(uglify())
         .pipe(dest("src/js"))
